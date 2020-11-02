7ece60c0b9306b66af8f37b21a453aac9fb6043d
sevtov <73488299+sevtov@users.noreply.github.com>
2020-11-02 23:43:48 +0200

    Update cube-script.js

--- cube-script.js
+++ cube-script.js
@@ -1,40 +1,22 @@
-$(function () {
-	var scene = new THREE.Scene();
-	var camera = new THREE.PerspectiveCamera(45 
-		, window.innerWidth / window.innerHeight , 0.1, 1000);
-	var renderer = new THREE.WebGLRenderer();
-	renderer.setClearColorHex(0xEEEEEE);
-	renderer.setSize(window.innerWidth, window.innerHeight);
-	var axes = new THREE.AxisHelper( 20 );
-	scene.add(axes);
-	var planeGeometry = new THREE.PlaneGeometry(60,20,1,1);
-	var planeMaterial = new THREE.MeshBasicMaterial({color: 0xcccccc});
-	var plane = new THREE.Mesh(planeGeometry,planeMaterial);
-	plane.rotation.x=-0.5*Math.PI;
-	plane.position.x = 15;
-	plane.position.y = 0;
-	plane.position.z = 0;
-	scene.add(plane);
-	var cubeGeometry = new THREE.CubeGeometry(4,4,4);
-	var cubeMaterial = new THREE.MeshBasicMaterial(
-		{color: 0xff0000, wireframe: true});
-	var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-	cube.position.x = -4;
-	cube.position.y = 3;
-	cube.position.z = 0;
-	scene.add(cube);
-	var sphereGeometry = new THREE.SphereGeometry(4,20,20);
-	var sphereMaterial = new THREE.MeshBasicMaterial(
-		{color: 0x7777ff, wireframe: true});
-	var sphere = new THREE.Mesh(sphereGeometry,sphereMaterial);
-	sphere.position.x = 20;
-	sphere.position.y = 4;
-	sphere.position.z = 2;
-	scene.add(sphere);
-	camera.position.x = -30;
-	camera.position.y = 40;
-	camera.position.z = 30;
-	camera.lookAt(scene.position);
-	$("#WebGL-output").append(renderer.domElement);
-	renderer.render(scene, camera);
-});
+var scene = new THREE.Scene();
+
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+var geometry = new THREE.BoxGeometry( 10, 10, 10);
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 25;
+
+function render() {
+        requestAnimationFrame( render );
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;      
+  renderer.render( scene, camera );
+}
+render();
